d6d3a8917a5c89e5cbc4a43e11d39b380f435cbd
Laura Damaceno de Almeida <41930302+lauraDamacenoAlmeida@users.noreply.github.com>
2019-07-30 10:04:48 -0300

    Update classificador.py

--- classificador.py
+++ classificador.py
@@ -1,12 +1,29 @@
-from textblob import TextBlob as tb
-import tweepy
-import numpy as np
+from TwitterSearch import *
+import json
 
-consumer_key = 'XXXXX'
-consumer_secret = 'XXXXXX'
-access_token = 'XXXXX'
-access_token_secret = 'XXXXX'
+try:
+    tweets = TwitterSearch(
+        consumer_key='Xxxxx',
+        consumer_secret='XXXX',
+        access_token='XXXXX',
+        access_token_secret='XXXXXXXX'
+    )
+    pesquisa = TwitterSearchOrder()
+    pesquisa.set_keywords(['IBM'])
+    pesquisa.set_language('pt')
+    tweetss = []
+    for tweet in tweets.search_tweets_iterable(pesquisa):
+        with open('tweets.json', 'a') as arq:
+            json.dump(tweet, arq, indent=4)
+        #tweetss.append({
+         #   tweet
+        #    "usuario": tweet["user"]["screen_name"],
+        #    "tweet": tweet['text']
+        #})
+       #print(tweet)
+        #tweetss.append('usuario:{0} tweet:{1}'.format({tweet['user']['screen_name']},tweet['text']))
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+
+except TwitterSearchException as e:
+    print(e)
+
